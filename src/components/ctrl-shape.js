6a98a4bd17abe827eb263aebff9e99cfac2d4a93
gantrylau <gantrylau@qq.com>
2016-12-28 23:52:45 +0800

    加入外框

--- src/components/ctrl-shape.js
+++ src/components/ctrl-shape.js
@@ -4,8 +4,8 @@
     class CtrlShape extends createjs.Shape {
         constructor() {
             super();
-            this.sx = 1;
-            this.sy = 1;
+            this.sx = this.scaleX;
+            this.sy = this.scaleY;
 
             let self = this;
             this.on('mousedown', function (event) {
@@ -29,7 +29,7 @@
 
         update(x, y) {
             if (x == undefined || this.state == 'still') {
-                this.updateSelf();
+                // this.updateSelf();
                 return;
             }
 
@@ -39,6 +39,7 @@
             } else {
                 this.dx = x - this.x;
                 this.dy = y - this.y;
+                console.log(this.dx, this.dy);
                 if (this.state == 'scale') {
                     this.scale();
                 } else {
@@ -62,9 +63,10 @@
             halfH = (this.height >> 1) * this.sy;
 
             for (let i = 0; i < 9; i++) {
-                row        = Math.floor(i / 3);
-                col        = Math.floor(i % 3);
-                circles[i] = new createjs.Point((col - 1) * halfW, (row - 1) * halfH);
+                row = Math.floor(i / 3);
+                col = Math.floor(i % 3);
+                circles[i] = new createjs.Point(col * halfW, row * halfH);
+                console.log(circles[i])
             }
 
             if (this.circles) {
@@ -74,7 +76,7 @@
         }
 
         drawCircles() {
-            let circle  = null;
+            let circle = null;
             let circles = this.circles;
 
             this.graphics.clear();
@@ -84,8 +86,8 @@
 
             graphics.setStrokeStyle(0.5).beginStroke("#0af");//beginStroke("#444");
 
-            graphics.drawRect(circles[0].x, circles[0].y, circles[8].x << 1, circles[8].y << 1);
-
+            graphics.drawRect(circles[0].x, circles[0].y, this.width * this.sx, this.height * this.sy);
+            //
             for (let i = 0; i < this.circles.length; i++) {
                 circle = circles[i];
                 graphics.beginFill('white').drawCircle(circle.x, circle.y, circleR).endFill();
@@ -124,8 +126,6 @@
             this.dx = x - this.x;
             this.dy = y - this.y;
 
-            console.log(this.dx, this.dy);
-
             let circles = this.circles;
 
             let theta = this.rotation * Math.PI / 180;
@@ -133,20 +133,30 @@
             let dx = this.dx * Math.cos(-theta) - this.dy * Math.sin(-theta);
             let dy = this.dx * Math.sin(-theta) + this.dy * Math.cos(-theta);
 
-            if (Math.abs(dx) > (this.width >> 1) * Math.abs(this.sx) + circleR || Math.abs(dy) > (this.height >> 1) * Math.abs(this.sy) + circleR) {
-                this.activeIndex = -1;
-            } else {
-                this.activeIndex = 4;
-                for (let i = 0; i < 9; i++) {
-                    if (i == 4) {
-                        continue;
-                    }
-                    if (Math.abs(dx - circles[i].x) < circleR && Math.abs(dy - circles[i].y) < circleR) {
-                        this.activeIndex = i;
-                        break;
-                    }
+            // if (Math.abs(dx) > (this.width >> 1) * Math.abs(this.sx) + circleR || Math.abs(dy) > (this.height >> 1) * Math.abs(this.sy) + circleR) {
+            //     this.activeIndex = -1;
+            // } else {
+            //     this.activeIndex = 4;
+            //     for (let i = 0; i < 9; i++) {
+            //         console.log(circles[i]);
+            //         if (i == 4) {
+            //             continue;
+            //         }
+            //         if (Math.abs(dx - circles[i].x) < circleR && Math.abs(dy - circles[i].y) < circleR) {
+            //             this.activeIndex = i;
+            //             break;
+            //         }
+            //     }
+            // }
+            this.activeIndex = 4;
+            for (let i = 0; i < 9; i++) {
+                if (Math.abs(dx - circles[i].x) < circleR && Math.abs(dy - circles[i].y) < circleR) {
+                    console.log(circles[i]);
+                    this.activeIndex = i;
+                    break;
                 }
             }
+            console.log(this.dx, this.dy, this.activeIndex);
 
             switch (this.activeIndex) {
 
@@ -182,20 +192,21 @@
             let col = Math.floor(this.activeIndex % 3);
 
             let theta = this.rotation * Math.PI / 180;
-            let cos   = Math.cos(-theta);
-            let sin   = Math.sin(-theta);
-            console.log(cos, sin);
+            let cos = Math.cos(-theta);
+            let sin = Math.sin(-theta);
             let dx = this.dx * cos - this.dy * sin;
             let dy = this.dx * sin + this.dy * cos;
 
-            if (col != 1) {
-                this.sx = (dx * (col - 1) / (this.width >> 1));
-            }
+            // if (col != 1) {
+            //     this.sx = (dx * (col - 1) / (this.width >> 1));
+            // }
+            //
+            // if (row != 1) {
+            //     this.sy = dy * (row - 1) / (this.height >> 1);
+            // }
 
-            if (row != 1) {
-                this.sy = dy * (row - 1) / (this.height >> 1);
-            }
-
+            this.sx = dx / this.width;
+            console.log(this);
             this.updateCircles();
             this.drawCircles();
 
@@ -208,7 +219,7 @@
             } else {
                 delta = Math.PI * (this.sx < 0 ? 0 : 1);
             }
-            let theta     = Math.atan2(this.dy, this.dx) + delta;
+            let theta = Math.atan2(this.dy, this.dx) + delta;
             this.rotation = theta * 180 / Math.PI;
         };
 
--- src/components/my-canvas.vue
+++ src/components/my-canvas.vue
@@ -1,5 +1,9 @@
 <template>
-    <canvas id="myCanvas" height="800" width="800"></canvas>
+    <div>
+        <canvas id="myCanvas" height="800" width="800"></canvas>
+        <button @click="toPic()">to</button>
+        <a :src="testSrc">ssss</a>
+    </div>
 </template>
 <script>
     import {mapState} from 'vuex'
@@ -9,23 +13,30 @@
     export default {
         data() {
             return {
-                lastPoint  : {
+                lastPoint: {
                     x: null,
                     y: null
                 },
+                testSrc:null,
                 mouseDowned: false
             }
         },
-        mounted : function () {
+        mounted: function () {
 
             this.createStage('myCanvas');
 
             let preload = new createjs.LoadQueue(true, null, true);
 
-//            preload.addEventListener("complete", this.loadCompleted);
+            preload.on('complete', this.loadCompleted);
 //            preload.loadFile({id: 'test', src: 'http://cdn-img.easyicon.net/png/5342/534223.gif'});
+            preload.loadFile({
+                id: 'test',
+                src: 'https://img.alicdn.com/imgextra/i4/92779311/TB2acZqXHplpuFjSspiXXcdfFXa-92779311.png'
+            });
 
-            let loadItem = new createjs.LoadItem().set({src: "http://cdn-img.easyicon.net/png/5342/534223.gif", crossOrigin: true});
+//            let item = new createjs.LoadItem().set({src: "http://cdn-img.easyicon.net/png/5342/534223.gif", crossOrigin: true});
+
+//            preload.load(item);
 //            let circle = new createjs.CtrlShape();
 //            circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 100).endFill();
 //            circle.width = circle.height = 200;
@@ -38,18 +49,59 @@
             this.stage.update();
         },
         computed: mapState({
-            stage      : state => state.myCanvas.stage,
-            children   : state => state.myCanvas.children,
+            stage: state => state.myCanvas.stage,
+            children: state => state.myCanvas.children,
             currentItem: state => state.myCanvas.currentItem
         }),
-        methods : {
+        methods: {
             ...mapMutations([
                 'createStage', 'startDrag', 'stopDrag', 'updateLastPoint', 'updateCurrentItem'
             ]),
-            loadCompleted    : function (event) {
-                let img      = event.target.getResult('test');
+            toPic:function() {
+                let u = document.getElementById('myCanvas').toDataURL();
+                let self = this;
+                function base64Img2Blob(code){
+                    var parts = code.split(';base64,');
+                    var contentType = parts[0].split(':')[1];
+                    var raw = window.atob(parts[1]);
+                    var rawLength = raw.length;
+
+                    var uInt8Array = new Uint8Array(rawLength);
+
+                    for (var i = 0; i < rawLength; ++i) {
+                        uInt8Array[i] = raw.charCodeAt(i);
+                    }
+
+                    return new Blob([uInt8Array], {type: contentType});
+                }
+                function downloadFile(fileName, content){
+
+                    var aLink = document.createElement('a');
+                    var blob = base64Img2Blob(content); //new Blob([content]);
+
+                    var evt = document.createEvent("HTMLEvents");
+                    evt.initEvent("click", false, false);//initEvent 不加后两个参数在FF下会报错
+                    aLink.download = fileName;
+                    aLink.href = URL.createObjectURL(blob)+'.png';
+
+                    aLink.dispatchEvent(evt);
+
+                    self.testSrc = URL.createObjectURL(blob);
+                }
+                downloadFile('test.png', u);
+            },
+            loadCompleted: function (event) {
+                let img = event.target.getResult('test');
                 let imgShape = new createjs.CtrlShape();
-                imgShape.graphics.beginBitmapFill(img).drawRect(0, 0, 100, 100);
+                let w = img.naturalWidth;
+                let h = img.naturalHeight;
+                imgShape.graphics.beginBitmapFill(img).drawRect(0, 0, w, h).endFill();
+                imgShape.width = w;
+                imgShape.height = h;
+                imgShape.x = 200;
+                imgShape.y = 200;
+                this.initEventListener(imgShape);
+                imgShape.showCtrl();
                 this.stage.addChild(imgShape);
                 this.stage.update();
             },
